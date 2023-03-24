a60790087f5c690102921af75c9d650dd1e1a95c
Matthew Hawes <55256507+mwh273@users.noreply.github.com>
2023-03-24 03:36:59 +0000

    Main pull request

--- BMI.cpp
+++ BMI.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main() 
 {
-  cout << "Hello World!";
+  string height = "";
+  string height_in = "";
+  int weight;
+
+  cout << "Hello this is the start of my BMI Calculator Program: \n";
+  cout << "Please enter in your height in feet(ex: 5ft): ";
+  cin >> height;
+  cout << "\nNext, please enter in your height in inches after feet: (if you are 5 ft 4 in you would enter in 4in): ";
+  cin >> height_in;
+  cout << "\nFinally, please enter in your weight in pounds (lbs): ";
+  cin >> weight;
+
+  cout << "\n" << height << " and " << height_in << " with " << weight <<  "lbs\n";
+
+
+
   return 0;
 }
