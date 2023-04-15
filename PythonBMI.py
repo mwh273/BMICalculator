64a2c13047a45d60a24c15d4800bfadaed9bd70e
Matthew Hawes <55256507+mwh273@users.noreply.github.com>
2023-04-15 12:41:13 -0500

    Update PythonBMI.py

--- PythonBMI.py
+++ PythonBMI.py
@@ -1,18 +1,3 @@
-#void main() 
-#{
-  #stringstream ftss;
-  #stringstream inss;
-  #string height = "";
-  #string height_in = "";
-  #int weight;
-  #int ft = 0;
-  #int in = 0;
-  #float new_weight = 0.0;
-  #float new_height = 0.0;
-  #float BMI = 0.0;
-  
-
-
 print("Hello this is the start of my BMI Calculator Program: \n")
   
 height = input("Please enter in your height in feet(ex: 5ft): ")
@@ -23,27 +8,27 @@ weight = input("\nFinally, please enter in your weight in pounds (lbs): ")
 
 print("\n" + height + " and " + height_in + " with " + weight +  "lbs\n")
 
-#new_weight = weight * 0.45;
-#ftss << height[0];
-#ftss >> ft;
-#inss << height_in[0];
-#inss >> inches;
-# cout << ft << "\t" << in << "\n";
-#new_height = ((ft * 12) + inches) * 0.025;
-#new_height = new_height * new_height;
-#BMI = new_weight / new_height;
+new_weight = int(weight) * 0.45;
 
-#rint("\nThe BMI is: " + BMI + "\n")
+ft = height[0]
 
-#if BMI >= 30:
-#  print("You are Obese.\n")
-#elif BMI < 30 and BMI >= 25:
-#  print("You are Overweight.\n")
-#elif BMI < 25 and BMI >= 18.5:
-#  print("You are Normal weight.\n")
-#elif BMI < 18.5:
-#  print("You are Underweight.\n")
+inches = height_in[0];
 
-  #return 0;
-#}
+new_height = ((int(ft) * 12) + int(inches)) * 0.025;
+new_height = new_height * new_height;
+
+BMI = new_weight / new_height;
+
+print("\nThe BMI is: " + str(BMI) + "\n")
+
+if BMI >= 30:
+  print("You are Obese.\n")
+elif BMI < 30 and BMI >= 25:
+  print("You are Overweight.\n")
+elif BMI < 25 and BMI >= 18.5:
+  print("You are Normal weight.\n")
+elif BMI < 18.5:
+  print("You are Underweight.\n")
+
+
 
